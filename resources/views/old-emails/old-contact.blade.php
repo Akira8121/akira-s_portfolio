f9c9bb6fae5bbbe5572f447bc040876578e3ea97
Akira8121 <akira.m2146@gmail.com>
2025-09-21 16:03:23 +0900

    Delete resources/views/old-emails directory

--- resources/views/old-emails/old-contact.blade.php
+++
@@ -1,9 +0,0 @@
-<p>以下の内容でお問い合わせがありました。</p>
-
-<p><strong>お名前:</strong> {{ $data['name'] }}</p>
-<p><strong>メール:</strong> {{ $data['email'] }}</p>
-<p><strong>電話番号:</strong> {{ $data['phone'] }}</p>
-<p><strong>性別:</strong> {{ $data['gender'] }}</p>
-<p><strong>件名:</strong> {{ $data['subject'] }}</p>
-<p><strong>メッセージ:</strong></p>
-<p>{{ $data['message'] }}</p>
